5667c228c81c61004dd82c5cf20ee0a7870529b4
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-05-29 20:33:14 +1000

    Added fields to the user model

--- models/User.js
+++ models/User.js
@@ -4,6 +4,31 @@ const sequelize = require('../config/connection');
 
 class User extends Model { }
 
-User.init();
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING(64),
+            allowNull: false
+        }
+    }
+);
 
 module.exports = User
