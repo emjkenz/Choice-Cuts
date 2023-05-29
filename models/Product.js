de6004980a9345c7a834fc0e21c5a5f769821bd7
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-05-29 20:24:27 +1000

    Added base models

---
+++ models/Product.js
@@ -0,0 +1,9 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class Product extends Model { }
+
+Product.init();
+
+module.exports = Product
