344ba1356f869ac19b84b714bf3ca35466dc7d9a
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-05-30 20:21:26 +1000

    Linked Category and Product forign keys

--- models/index.js
+++ models/index.js
@@ -3,6 +3,16 @@ const Product = require('./Product');
 const User = require('./User');
 const Category = require('./Category');
 
+// Categories have many Products
+Category.hasMany(Product, {
+    foreignKey: 'category_id'
+});
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+    foreignKey: 'category_id'
+});
+
 module.exports = {
     Product,
     User,
