5f59fa3ca2cafcd9b319d1ac3845d561336b43b0
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-06-03 18:12:40 +1000

    Added current year helper

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const exphbs = require('express-handlebars');
 
 const routes = require('./routes');
 const sequelize = require('./config/connection');
-//const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -20,8 +20,7 @@ const sess = {
 
 app.use(session(sess));
 
-//const hbs = exphbs.create({ helpers });
-const hbs = exphbs.create({ });
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
